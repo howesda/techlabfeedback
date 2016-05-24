f25ebda16468c99f91b9327dbd8987224dce6988
alice.montgomery <alice.montgomery@dwp.gsi.gov.uk>
2016-05-24 13:06:13 +0100

    feedback

---
+++ src/main/webapp/js/FeedbackForm.js
@@ -0,0 +1,71 @@
+	var happyCount=0, indifferentCount=0, sadCount=0;
+
+	$(function(){
+		update();
+		$('.votingButtons').hide();
+		$('.results').hide();
+	});
+	
+	function renderChart() {
+		var chart = new CanvasJS.Chart("chartContainer", {
+			theme: "theme2",
+			colorSet: "colorSet3",
+			title:{
+				text: "Tech Talk Feedback Results"              
+			},
+			data: [              
+			{
+				type: "column",
+				dataPoints: [
+					{ label: "Happy",  y: happyCount  },
+					{ label: "Indifferent", y: indifferentCount  },
+					{ label: "Unhappy", y: sadCount  }
+				]
+			}
+			]
+		});
+		chart.render();
+	}
+
+	function happy() {
+		happyCount++;
+		changeView()
+		update();
+	};
+	function indifferent() {
+		indifferentCount++;
+		changeView()
+		update();
+	};
+	function sad() {
+		sadCount++;
+		changeView()
+		update();
+	};
+	
+	function yes(){
+		showResults();
+	}
+	function no() {
+		showResults();
+	}
+	
+	function changeView() {
+		$('.votingFaces').hide();
+		$('.votingButtons').show();
+	}
+	function showResults() {
+		$('.votingButtons').hide();
+		$('.results').show();
+		setTimeout(function() {
+			$('.results').hide();
+			$('.votingFaces').show();
+	    }, 5000);
+	}
+	
+	function update() {
+		$('.happy').html(happyCount);
+		$('.indifferent').html(indifferentCount);
+		$('.sad').html(sadCount);
+		renderChart();
+	};
